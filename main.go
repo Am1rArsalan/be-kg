5718a9d59fbcebb3fa8f41dfdea6f3873d07fd0e
Amir <amirahmadzadehdev@gmail.com>
2024-10-16 19:01:16 +0200

    fix: cors;

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
